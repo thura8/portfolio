7c7ab896d3be4928af8dadc192ca47d1c4b04b1c
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-03-22 22:50:23 +0630

    First commit of portfolio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Home, About, Contact, Projects } from "./pages";
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
