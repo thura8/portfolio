c7c6e70a2458d5120ef329165d8bff4645e4a707
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-03-25 21:38:20 +0630

    Modify Homeinfo

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+const InfoBox = ({ text, link, btnText }) => {
+  return (
+    <div className="info-box">
+      <p className="font-medium sm:text-xl text-center">{text}</p>
+      <Link to={link} className="neo-brutalism-white neo-btn">
+        {btnText}
+        <img src={arrow} className="w-4 h-4 object-contain" />
+      </Link>
+    </div>
+  );
+};
 
 const renderContent = {
-  1: <h1>1</h1>,
-  2: <h1>2</h1>,
-  3: <h1>3</h1>,
-  4: <h1>4</h1>,
+  1: (
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+      Hi, I am <span className="font-semibold">Thu Ra Lynn Htun @ Steven</span>
+      👋
+      <br />A Software Developer from Myanmar
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text={
+        "I hone my skills through real-world projects, continuously improving my development expertise."
+      }
+      link={"/about"}
+      btnText={"Learn more"}
+    />
+  ),
+  3: (
+    <InfoBox
+      text={
+        "Contributed to various projects, focusing on learning, problem-solving, and delivering quality solutions"
+      }
+      link={"/projects"}
+      btnText={"Visit my portfolio"}
+    />
+  ),
+  4: (
+    <InfoBox
+      text={
+        "Ideas to bring to life? Code to be written? I’m just a click away!"
+      }
+      link={"/contact"}
+      btnText={"Let's talk"}
+    />
+  ),
 };
 
 const HomeInfo = ({ currentStage }) => {
