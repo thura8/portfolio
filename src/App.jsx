d250fb91ae7f6d65c285b9fb72bdb4245bbe44ec
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-03-29 22:36:36 +0630

    Modify the changes of height to App and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home, About, Contact, Projects } from "./pages";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20 h=[100vh]">
+    <main className="bg-slate-300/20 h-full">
       <Router>
         <NavBar />
         <Routes>
